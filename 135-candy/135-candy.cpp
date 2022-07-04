d6491e9816e0df394ed29334e4de1fbe122352cd
Prateek Goyal <68554362+gprateek015@users.noreply.github.com>
2022-07-04 13:06:59 +0530

    Time: 33 ms (52.60%), Space: 17.8 MB (40.68%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -7,9 +7,7 @@ public:
         for(int i=1; i<ratings.size(); i++) {
             if(ratings[i]>ratings[i-1]) {
                 arr[i] = arr[i-1]+1;
-            } else {
-                arr[i] = 1;
-            }
+            } 
         }
         for(int i=ratings.size()-2; i>=0; i--) {
             if(ratings[i]>ratings[i+1] && arr[i]<=arr[i+1]) {
