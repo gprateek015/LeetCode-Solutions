11ac6b4dbb1214926671580e4772cf8e9622e73b
Prateek Goyal <68554362+gprateek015@users.noreply.github.com>
2022-07-28 23:03:39 +0530

    Time: 6 ms (90.64%), Space: 7.2 MB (77.97%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int arr[26] = {0};
+        for(char &c: s) {
+            arr[c-'a']++;
+        }
+        for(char &c: t) {
+            arr[c-'a']--;
+        }
+        for(int i=0; i<26; i++) {
+            if(arr[i]!=0) return false;
+        }
+        return true;
+    }
+};
