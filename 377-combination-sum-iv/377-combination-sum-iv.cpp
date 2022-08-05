4c4821a12ac2aa1f75568914f0b370b97245f978
Prateek Goyal <68554362+gprateek015@users.noreply.github.com>
2022-08-05 11:55:06 +0530

    Time: 6 ms (27.16%), Space: 6.6 MB (91.95%) - LeetHub

---
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    vector<int> dp;
+    int helper(vector<int>& nums, int target) {
+        if(target==0) return 1;
+        if(target < 0) return 0;
+        if(dp[target]!=-1)
+            return dp[target];
+        int ans=0;
+        for(int i=0; i<nums.size(); i++) {
+            ans += helper(nums, target-nums[i]);
+        }
+        return dp[target] = ans;
+    }
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        dp = vector<int>(target+1, -1);
+        return helper(nums, target);
+    }
+};
