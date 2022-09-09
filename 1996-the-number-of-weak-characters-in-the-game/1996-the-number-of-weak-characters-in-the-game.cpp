2bab7d9a6aa1bc1a8dd5895615e36c4fcb137362
Prateek Goyal <68554362+gprateek015@users.noreply.github.com>
2022-09-09 22:36:35 +0530

    Time: 776 ms (83.13%), Space: 125.2 MB (70.41%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int numberOfWeakCharacters(vector<vector<int>>& prop) {
-        sort(prop.begin(), prop.end());
-        vector<int> arr(prop.size(), 0);
-        int maxi = prop[prop.size()-1][1];
-        int oldMaxi = 0;
-        int maxi2 = prop[prop.size()-1][0];
-        for(int i=prop.size()-1; i>=0; i--) {
-            maxi = max(maxi, prop[i][1]);
-            int j = i;
-            while(j>=0 && prop[i][0] == prop[j][0]) {
-                arr[j] = oldMaxi;
-                j--;
-            }
-            i = j+1;
-            oldMaxi = maxi;
-        }
-        int ans = 0;
-        for(int i=0; i<prop.size(); i++) {
-            if(prop[i][1] < arr[i]) {
-                ans++;
-            }
-        }
-        return ans;
+    static bool cmp(vector<int>&a,vector<int>&b){
+       if(a[0]==b[0]) 
+           return a[1]<b[1];
+       return(a[0]>b[0]);
+    }
+    
+    int numberOfWeakCharacters(vector<vector<int>>&properties)  {
+        sort(properties.begin(),properties.end(), cmp);
+        int temp=properties[0][1],res=0;
+        for(int i=1;i<properties.size();i++){
+            if(properties[i][1]<temp) 
+                res++;
+            else temp=properties[i][1];    
+        } 
+        return res; 
     }
 };
