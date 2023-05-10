f0505832528fd59ebb875eb41bf4a0703ab10623
Prateek Goyal <68554362+gprateek015@users.noreply.github.com>
2023-05-10 23:41:01 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (23.20%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -5,18 +5,14 @@ public:
         int l=0;
         int num = 1;
         while(l<n) {
-            for(int i=0+l; i<n-l; i++) {
+            for(int i=0+l; i<n-l; i++)
                 ans[l][i] = num++;;
-            }
-            for(int i=l+1; i<n-l; i++) {
+            for(int i=l+1; i<n-l; i++)
                 ans[i][n-l-1] = num++;
-            }
-            for(int i=n-l-2; i>=0+l; i--) {
+            for(int i=n-l-2; i>=0+l; i--)
                 ans[n-l-1][i] = num++;
-            }
-            for(int i=n-l-2; i>=0+l+1; i--) {
+            for(int i=n-l-2; i>=0+l+1; i--)
                 ans[i][l] = num++;
-            }
             l++;
         }
         return ans;
