5724f120503be28a4de21a6f0d62d675a9825291
Prateek Goyal <68554362+gprateek015@users.noreply.github.com>
2023-08-07 19:09:52 +0530

    Time: 3 ms (82.89%), Space: 9.6 MB (19.73%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,14 +5,12 @@ public:
         int s=0, end=matrix.size()-1;
         while(s<=end) {
             int m = (s+end)/2;
-            if(matrix[m][0] == target) {
-                return true;
-            } else if(matrix[m][0] > target) {
-                end = m-1;
-            } else {
+            if(matrix[m][0] <= target) {
                 r = m;
                 s = m+1;
-            }
+            } else {
+                end = m-1;
+            } 
         }
         if(r<0) return false;
         return binary_search(matrix[r].begin(), matrix[r].end(), target);
